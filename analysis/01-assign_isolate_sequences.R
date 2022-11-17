00eb60516628680f6c09910e9bf785e0cff68f90
Chang-Yu Chang <cychang@dept-dynamic-host-222-117.bio.upenn.edu>
2022-11-16 19:12:29 -0500

    isolate sanger

---
+++ analysis/01-assign_isolate_sequences.R
@@ -0,0 +1,209 @@
+#' This scripts reads and aligns raw sanger sequences from Genewiz
+
+if (FALSE) {
+    install.packages("BiocManager") # Package for managing and installing Bioconductor packages
+    BiocManager::install("sangeranalyseR")
+}
+
+library(tidyverse)
+library(sangeranalyseR)
+folder_data <- here::here("data/")
+#source(here::here("analysis/00-metadata.R"))
+
+
+# 1. Align the F and R reads ----
+alignment1 <- sangeranalyseR::SangerAlignment(
+    ABIF_Directory = paste0(folder_data, "raw/sanger/1st Round/"),
+    REGEX_SuffixForward = "F.ab1$",
+    REGEX_SuffixReverse = "R.ab1$"
+)
+
+#' This generates three merged fasta files:
+#' 1. Sanger_all_trimmed_reads.fa
+#' 2. Sanger_contigs_alignment.fa
+#' 3. Sanger_contigs_unalignment.fa
+
+sangeranalyseR::writeFasta(
+    alignment1,
+    outputDir = paste0(folder_data, "raw/sanger/1st Round"),
+    compress = FALSE,
+    compression_level = NA,
+    selection = "all"
+)
+
+# Repeat it for the 2rd round
+alignment2 <- sangeranalyseR::SangerAlignment(
+    ABIF_Directory = paste0(folder_data, "raw/sanger/2nd Round/"),
+    REGEX_SuffixForward = "F.ab1$",
+    REGEX_SuffixReverse = "R.ab1$"
+)
+sangeranalyseR::writeFasta(
+    alignment2,
+    outputDir = paste0(folder_data, "raw/sanger/2nd Round"),
+    compress = FALSE,
+    compression_level = NA,
+    selection = "all"
+)
+
+
+
+#
+consensus1 <- seqinr::read.fasta(paste0(folder_data, "raw/sanger/1st Round/Sanger_all_trimmed_reads.fa"), seqtype = "DNA")
+consensus2 <- seqinr::read.fasta(paste0(folder_data, "raw/sanger/2nd Round/Sanger_all_trimmed_reads.fa"), seqtype = "DNA")
+#consensus1 <- seqinr::read.fasta(paste0(folder_data, "raw/sanger/1st Round/Sanger_contigs_alignment.fa"), seqtype = "DNA")
+#consensus2 <- seqinr::read.fasta(paste0(folder_data, "raw/sanger/2nd Round/Sanger_contigs_alignment.fa"), seqtype = "DNA")
+clean_concensus <- function(x) {
+    contig <- x %>% as.character()
+    contig[contig=="a"] <- "A"
+    contig[contig=="t"] <- "T"
+    contig[contig=="g"] <- "G"
+    contig[contig=="c"] <- "C"
+    contig <- paste(contig, collapse = "")
+    contig <- gsub("-", "", contig)
+    return(data.frame(Sequence = contig))
+}
+consensus1 <- consensus1 %>% lapply(clean_concensus)
+consensus2 <- consensus2 %>% lapply(clean_concensus)
+
+df_seq1 <- bind_rows(consensus1, .id = "temp") %>%
+    tidyr::separate(col = "temp", into = c("ID", "t1", "t2", "t3"), sep = "-") %>%
+    dplyr::select(ID, Sequence) %>%
+    dplyr::mutate(ID = str_replace(ID, "Wood06_", "") %>% as.numeric() %>% factor(levels = 1:100)) %>%
+    dplyr::mutate(ConsensusLength = nchar(Sequence)) %>%
+    as_tibble() %>%
+    arrange(ID)
+
+df_seq2 <- bind_rows(consensus2, .id = "temp") %>%
+    tidyr::separate(col = "temp", into = c("ID", "t1", "t2", "t3"), sep = "-") %>%
+    dplyr::select(ID, Sequence) %>%
+    dplyr::mutate(ID = str_replace(ID, "Wood06_", "") %>% as.numeric() %>% factor(levels = 1:100)) %>%
+    dplyr::mutate(ConsensusLength = nchar(Sequence)) %>%
+    as_tibble() %>%
+    arrange(ID)
+
+df_seq <- filter(df_seq1, !(df_seq1$ID %in% df_seq2$ID)) %>%
+    bind_rows(df_seq2) %>%
+    arrange(ID) %>%
+    distinct(ID, .keep_all = T)
+
+write_csv(df_seq, paste0(folder_data, "temp/01-isolates_16S.csv"))
+
+# 2. RDP ----
+#' This scripts reads isolate 16S sequences and assigns taxonomy using RDP
+if (FALSE) {
+    BiocManager::install("rRDPData")
+    BiocManager::install("rRDP")
+}
+
+library(tidyverse)
+library(rRDPData)
+library(rRDP)
+library(Biostrings)
+#source(here::here("analysis/00-metadata.R"))
+isolates_ID <- read_csv(paste0(folder_data, "raw/rhizobia/isolates_for_seq.csv"), col_types = cols()) %>%
+    mutate(ID = as.numeric(`Sample Name`)) %>%
+    select(ExpID, ID) %>%
+    mutate(ExpID = str_replace(ExpID, " ", ""))
+
+# Read 16S sequence
+isolates_16S <- read_csv(paste0(folder_data, "temp/01-isolates_16S.csv"), col_types = cols()) %>%
+    right_join(isolates_ID, by = "ID") %>%
+    select(ExpID, ID, Sequence) %>%
+    filter(!is.na(Sequence))
+
+# Make DNA string set object
+isolates_seq_set <- DNAStringSet(isolates_16S$Sequence)
+names(isolates_seq_set) <- isolates_16S$ExpID
+
+# Use rdp for classification (this needs package rRDPData)
+pred <- predict(rdp(), isolates_seq_set, confidence = 0)
+conf_score <- attr(pred, "confidence") %>% as.data.frame()
+colnames(pred) <- c("Domain", "Phylum", "Class", "Order", "Family", "Genus")
+colnames(conf_score) <- paste0(c("Domain", "Phylum", "Class", "Order", "Family", "Genus"), "Score")
+pred$ExpID <- rownames(pred)
+conf_score$ExpID <- rownames(pred)
+
+# Join the predicted taxonomy and isolates information
+isolates_RDP <- left_join(isolates_16S, pred, by = "ExpID") %>% left_join(conf_score, "ExpID")
+write_csv(isolates_RDP, paste0(folder_data, "temp/02-isolates_RDP.csv"))
+
+
+# 3. Plot ----
+library(cowplot)
+isolates_RDP <- read_csv(paste0(folder_data, "temp/02-isolates_RDP.csv"), show_col_types = F) %>%
+    #arrange(Genus) %>%
+    mutate(Genus = factor(Genus, levels = rev(unique(isolates_RDP$Genus)))) %>%
+    mutate(Family = factor(Family, levels = rev(unique(isolates_RDP$Family)))) %>%
+    filter(!is.na(Family))
+
+p1 <- isolates_RDP %>%
+    ggplot() +
+    geom_bar(aes(y = Family, fill = Family), color = 1) +
+    annotate("text", x = Inf, y = Inf, label = paste0("N=", nrow(isolates_RDP)), vjust = 2, hjust = 2) +
+    scale_fill_brewer(palette = "Set1") +
+    theme_classic() +
+    theme(panel.grid.major.x = element_line(color = grey(0.8), linetype = 1))
+    #guides(fill = "none")
+
+p2 <- isolates_RDP %>%
+    ggplot() +
+    geom_bar(aes(y = Genus, fill = Family), color = 1) +
+    annotate("text", x = Inf, y = Inf, label = paste0("N=", nrow(isolates_RDP)), vjust = 2, hjust = 2) +
+    scale_fill_brewer(palette = "Set1") +
+    theme_classic() +
+    theme(panel.grid.major.x = element_line(color = grey(0.8), linetype = 1))
+
+
+p <- plot_grid(p1, p2, nrow = 2, axis = "lr", align = "v")
+
+ggsave(here::here("plots/01-isolate_taxonomy.png"), plot = p, width = 8, height = 7)
+
+# Isolate ID distribution
+isolates_ID <- isolates_ID %>%
+    mutate(Site = ExpID %>% str_sub(1, 3) %>% str_replace("-", "") %>% str_replace("M", "") %>%
+               str_replace("fp1|fp2", "fp") %>% str_replace("gp1", "gp")) %>%
+    mutate(Owner = ifelse(str_sub(Site, 1, 1) %in% c("L", "H"), "CYC", "TPP"))
+
+site_id <- unique(isolates_ID$Site)
+family_id <- c("Rhizobiaceae", "Pseudomonadaceae", "Enterobacteriaceae", "Others")
+
+
+p <- isolates_RDP %>%
+    mutate(Family = as.character(Family)) %>%
+    mutate(Family = ifelse(Family %in% c("Rhizobiaceae", "Pseudomonadaceae", "Enterobacteriaceae"), Family, "Others")) %>%
+    left_join(isolates_ID) %>%
+    mutate(Site = factor(Site, site_id)) %>%
+    mutate(Family = factor(Family, family_id)) %>%
+    group_by(Owner, Site, Family) %>%
+    summarize(Count = n()) %>%
+    ggplot() +
+    geom_col(aes(x = Site, y = Count, fill = Family), color = 1) +
+    scale_fill_brewer(palette = "Set2") +
+    scale_y_continuous(breaks = 1:10) +
+    theme_classic()
+ggsave(here::here("plots/01-family_count.png"), plot = p, width = 6, height = 3)
+
+# Check the used rhizobia ----
+
+isolates_RDP %>%
+    left_join(isolates_ID) %>%
+    filter(Owner == "CYC", Genus == "Ensifer")
+
+
+# expid_used <- c("H1M2R3", "H3M1R1", "H4M1R1", "L1M3R2", "L2M2R1", "L3M1R1")
+# isolates_RDP %>%
+#     filter(ExpID %in% expid_used)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ analysis/generate_csv/generate_isolates_ID.R
@@ -0,0 +1,124 @@
+#' This scripts generate the csv table of isolates with glycerol stocks
+
+library(tidyverse)
+
+# 1. Generate local-urban isolate ID  ----
+isolates <- tibble(
+    IsolateID = c(
+        # 20221017
+        "L2M4R1", "L1M3R2", "L1M3R1", "L1M2R2", "L1M1R4", "L2M4R4", "L2M4R2", "L2M3R2", "L2M2R1", "L2M1R1",
+        "H4M5R2", "H4M1R1", "H3M4R1", "H3M3R2", "H3M3R1", "H3M2R1", "H3M1R1", "H2M3R1", "H2M2R1", "H4M5R1", "H2M4R1", "H2M3R2", "H1M2R3", "H1M2R1", "H1M1R1",
+        # 20221019
+        "L1M1R1", "L1M1R2", "L2M5R2", "L3M4R1",
+        "H1M3R1", "H1M4R1", "H1M2R2", "H2M2R2", "H3M3R4", "H3M4R2",
+        "L4M2R2", "L4M3R3", "L4M4R1", "L4M4R3", "L4M5R1",
+        "L3M2R1", "L3M2R2", "L3M3R1", "L3M5R1", "L3M5R2",
+        "L3M1R1", "L4M5R4", "L4M7R1", "L4M7R2", "L4M8R1",
+        "L4M5R2", "L3M6R2", "L3M6R1", "L3M4R3", "L3M4R2", "L2M2R2", "L1M4R2", "L1M2R4", "L1M2R3",
+        "H4M1R3",
+        # 20221020
+        "H4M2R2", "H4M4R2",
+        "L1M2R1", "L1M4R1", "L4M1R1", "L4M1R3", "L4M5R3"
+    )
+) %>%
+    mutate(Site = str_sub(IsolateID, 1, 1), SiteID = str_sub(IsolateID, 2, 2), PlantID = str_sub(IsolateID, 1, 4)) %>%
+    select(Site, SiteID, PlantID, IsolateID)
+
+isolates %>%
+    distinct(IsolateID)
+
+
+isolates %>%
+    arrange(IsolateID) %>%
+    group_by(PlantID) %>%
+    count(name = "Count")
+
+# Number of isolates at H and L
+isolates %>%
+    group_by(Site) %>%
+    count(name = "Count")
+
+# Number per sites
+isolates %>%
+    group_by(Site, SiteID) %>%
+    count(name = "Count")
+
+
+isolates <- isolates %>%
+    arrange(IsolateID) %>%
+    mutate(` `= 1:n()) %>%
+    select(` `, everything())
+
+
+write_csv(isolates, file = here::here("data/raw/isolates_ID.csv"))
+
+
+#
+isolate_cryo_L <- isolates %>%
+    filter(Site == "L") %>%
+    arrange(IsolateID) %>%
+    pull(IsolateID) %>%
+    c(rep("", 9*((length(.)%/%9)+1)-length(.))) %>%
+    matrix(ncol = 9, byrow = T) %>%
+    as_tibble() %>%
+    set_names(1:9)
+
+isolate_cryo_H <- isolates %>%
+    filter(Site == "H") %>%
+    arrange(IsolateID) %>%
+    pull(IsolateID) %>%
+    c(rep("", 9*((length(.)%/%9)+1)-length(.))) %>%
+    matrix(ncol = 9, byrow = T) %>%
+    as_tibble() %>%
+    set_names(1:9) %>%
+    bind_rows(isolate_cryo_L[5,])
+
+padding <- matrix("", ncol = 9) %>% as_tibble %>% set_names(1:9)
+isolate_cryo_L <- bind_cols(
+    tibble(` ` = 1:8),
+    bind_rows(
+        isolate_cryo_L[1,],
+        padding,
+        isolate_cryo_L[2,],
+        padding,
+        isolate_cryo_L[3,],
+        padding,
+        isolate_cryo_L[4,],
+        padding
+    )
+)
+isolate_cryo_H <- bind_cols(
+    tibble(` ` = 1:8),
+    bind_rows(
+        isolate_cryo_H[1,],
+        padding,
+        isolate_cryo_H[2,],
+        padding,
+        isolate_cryo_H[3,],
+        padding,
+        isolate_cryo_H[4,],
+        padding
+    )
+)
+write_csv(isolate_cryo_L, here::here("data/raw/isolates_cryo_L.csv"))
+write_csv(isolate_cryo_H, here::here("data/raw/isolates_cryo_H.csv"))
+
+
+
+# 2. Combine ID for my isolates and Terrence's isolates ----
+isolates_urban <- read_csv(here::here("data/raw/rhizobia/isolates_urban.csv"), show_col_types = F) %>%
+    filter(!is.na(StrainID))
+
+isolates_for_seq <- tibble(ExpID = c(isolates$IsolateID, isolates_urban$StrainID)) %>%
+    mutate(`Sample Name` = sprintf("%03.0f", 1:n()),
+           `# of picks per container` = 1,
+           Note = paste0("bag ", c(rep(1:5, each = 16), rep(6, 11))))
+
+write_csv(isolates_for_seq, here::here("data/raw/rhizobia/isolates_for_seq.csv"))
+
+
+c("Sample Name", "# of picks per container", "Note")
+
+
+
+
