a2482a6ebebe80ff957c815cae20efd82015c316
Chang-Yu Chang <cychang@dept-dynamic-host-222-117.bio.upenn.edu>
2023-02-07 14:00:21 -0500

    biomass

--- analysis/02-dry_weight.R
+++ analysis/02-dry_weight.R
@@ -2,13 +2,13 @@
 
 library(tidyverse)
 library(broom)
+library(lme4)
 source(here::here("analysis/00-metadata.R"))
 
 biomass <- read_csv(paste0(folder_data, "raw/rhizobia/04-phenotyping/treatments_assigned.csv"), show_col_types = F) %>%
     rename(DryWeight = `DryWeight (mg)`)
 
-#
-
+# Exploratory plots ----
 biomass %>%
     filter(!is.na(DryWeight)) %>%
     #replace_na(list(DryWeight = 0))
@@ -90,6 +90,74 @@ temp <- biomass %>%
 pairwise.t.test(temp$DryWeight, temp$Rhizobia, p.adjust.method = "bonferroni")
 
 
+# Check assumptions ----
+
+# Model
+#--------------------------------------------------------------#
+# Prepare workspace
+#--------------------------------------------------------------#
+# Load libraries
+# library(car)
+# library(lattice)
+library(lme4)
+# library(lsmeans)
+# library(glmmADMB)
+# Load script with custom functions to check assumptions of linear models and calculate VIFs
+source("~/Dropbox/lab/local-adaptation/data/test/Wood2018/checkAssumptions.R")
+experiment1 <- read_csv("~/Dropbox/lab/local-adaptation/data/test/Wood2018/Experiment1.csv")
+experiment2 <- read_csv("~/Dropbox/lab/local-adaptation/data/test/Wood2018/Experiment2.csv")
+
+# Set working directory
+#setwd("~/Documents/Post-doc/Medicago-Nematode MS/Data and code for Dryad/")
+
+# Set contrasts (sum-to-zero rather than R's default treatment contrasts)
+# http://rstudio-pubs-static.s3.amazonaws.com/65059_586f394d8eb84f84b1baaf56ffb6b47f.html
+#options(contrasts=c("contr.sum", "contr.poly"))
+
+# Read in the data: EXPERIMENT 1
+experiment1 = read.csv("Experiment1.csv")
+
+# Read in the data: EXPERIMENT 2
+experiment2 = read.csv("Experiment2.csv")
+
+
+
+#--------------------------------------------------------------#
+# Data analysis: EXPERIMENT 1
+#--------------------------------------------------------------#
+# THE EFFECT OF NODULES & GALLS ON FITNESS IN CO-INFECTED PLANTS ####
+# Aboveground biomass
+mod = lmer(log(AbovegroundMass.g) ~ Nodules + Galls + RootMass.g + scale(Number.of.Nematode.Eggs)
+           + Researcher + (1|Block), experiment1)
+Anova(mod, type=3)
+summary(mod)
+check.assumptions(mod) # Assumptions met
+vif.lme(mod)
+# Excluding plants that received too many nematode eggs does not qualitatively change the answer
+mod = lmer(AbovegroundMass.g ~ Nodules + Galls + RootMass.g + scale(Number.of.Nematode.Eggs)
+           + Researcher + (1|Block), subset(experiment1, Number.of.Nematode.Eggs<=400))
+
+
+
+
+
+
+if (FALSE) {
+#
+
+PlantGrowth %>%
+    as_tibble() %>%
+    ggplot(aes(x = group, y = weight)) +
+    geom_boxplot() +
+    geom_jitter() +
+    theme_classic()
+
+fit_plant <- aov(weight ~ group, data = PlantGrowth)
+summary(fit_plant)
+dummy.coef(fit_plant)
+coef(fit_plant)
+
+plot(fit_plant, which = 2)
 
 
 
@@ -107,12 +175,5 @@ pairwise.t.test(temp$DryWeight, temp$Rhizobia, p.adjust.method = "bonferroni")
 
 
 
-
-
-
-
-
-
-
-
+}
 
