5e74225910b78912255b20d724640751600bd89a
Chang-Yu Chang <cychang@dept-dynamic-host-222-117.bio.upenn.edu>
2023-10-26 17:28:19 -0400

    edit 30a shell script to correct output fir

--- shell_scripts/30a-nanocomp.sh
+++ shell_scripts/30a-nanocomp.sh
@@ -28,4 +28,4 @@ NanoComp --verbose \
 "$folder_raw_result/Chang_Q5C_results_repeated/Chang_Q5C_18/reads/raw_reads.fastq.gz" \
 "$folder_raw_result/Chang_Q5C_results/Chang_Q5C_19/reads/raw_reads.fastq.gz" \
 --names g1 g2 g3 g4 g5 g6 g7 g8 g9 g10 g11 g12 g13 g14 g15 g16 g17 g18 g19 \
---outdir "/Users/cychang/Dropbox/lab/local-adaptation/data/temp/plasmidsaurus"
+--outdir "/Users/cychang/Dropbox/lab/local-adaptation/data/temp/plasmidsaurus/summary/nanocomp"
