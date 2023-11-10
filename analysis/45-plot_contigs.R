2495693185135c7bfce9cc87a81318fb76b378d0
Chang-Yu Chang <cychang@dept-dynamic-host-222-117.bio.upenn.edu>
2023-11-10 11:44:28 -0500

    add heatmap and tree for genes on contigs

--- analysis/45-plot_contigs.R
+++ analysis/45-plot_contigs.R
@@ -36,25 +36,37 @@ egcc <- gene_calls %>%
     left_join(egc) %>%
     mutate(genome_name = factor(genome_name, genomes$genome_name), genome_id = factor(genome_id, genomes$genome_id))
 
-# 0.5 read the raw data
-
-
-
-# 1. plot the gene numbers on each contig
-p <- egcc %>%
-    distinct(gene_cluster_id, gene_callers_id, .keep_all = T) %>%
-    mutate(contig = str_remove_all(contig, "0")) %>%
+# 0.6 order the contigs by size
+egcc_count <- egcc %>%
+    #distinct(gene_cluster_id, gene_callers_id, .keep_all = T) %>%
     group_by(genome_name, contig) %>%
     summarize(n_genes = n()) %>%
     # Remove very small contigs
-    filter(n_genes > 10) %>%
+    #filter(n_genes > 10) %>%
     # Rename the contigs by size
     arrange(genome_name, desc(n_genes)) %>%
-    mutate(contig = paste0("contig_", 1:n())) %>%
+    mutate(contig_ordered = factor(paste0("c", 1:n()))) %>%
+    ungroup()
+
+# 0.7 label the contigs by new contig label
+egcc_ordered <- egcc %>%
+    left_join(select(egcc_count, genome_name, contig, contig_ordered)) %>%
+    #select(genome_name, genome_id, contig, contig_ordered) %>%
+    # Remove the genes on the small contigs
+    filter(!is.na(contig_ordered)) %>%
+    ungroup() %>%
+    mutate(contig_id = paste0(genome_id, "_", contig_ordered)) %>%
+    mutate(contig_id = factor(contig_id, paste0("g", rep(1:20, each = 20), "_", "c", rep(1:20, 20))))
+
+
+
+
+# 1. plot the gene numbers on each contig ----
+p <- egcc_count %>%
     mutate(n_genes = n_genes / 1000) %>%
     ggplot() +
-    geom_col(aes(x = genome_name, y = n_genes, fill = contig), color = "black", position = position_stack(reverse = T)) +
-    scale_fill_manual(values = RColorBrewer::brewer.pal(n = 9, "Set1")) +
+    geom_col(aes(x = genome_name, y = n_genes, fill = contig_ordered), color = "black", position = position_stack(reverse = T)) +
+    scale_fill_manual(values = c(RColorBrewer::brewer.pal(n = 9, "Set1"), RColorBrewer::brewer.pal(n = 8, "Set2"))) +
     scale_y_continuous(breaks = seq(0, 8, 2), minor_breaks = 1:10) +
     theme_classic() +
     theme(
@@ -65,18 +77,69 @@ p <- egcc %>%
     guides(fill = guide_legend(ncol = 1, reverse = T)) +
     labs(x = "genome", y = "# of genes (k)")
 
-ggsave(paste0(folder_data, "temp/45-01-contig_genes.png"), p, width = 6, height = 4)
+ggsave(paste0(folder_data, "temp/45-01-contig_genes_n.png"), p, width = 6, height = 4)
+
+# 2. plot the heatmap for gene presence absence
+p <- egcc_ordered %>%
+    filter(contig_ordered %in% paste0("c", 1:3)) %>%
+    ggplot() +
+    geom_rect(data = tibble(contig_ordered = paste0("c", 1:3)), xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf, aes(fill = contig_ordered), alpha = 0.2) +
+    geom_tile(aes(x = gene_cluster_id, y = genome_id), fill = "black") +
+    scale_fill_manual(values = c(RColorBrewer::brewer.pal(n = 9, "Set1"), RColorBrewer::brewer.pal(n = 8, "Set2"))) +
+    scale_y_discrete(position = "right") +
+    facet_wrap(contig_ordered~., scales = "free_y", ncol = 1, strip.position = "left") +
+    theme_classic() +
+    theme(
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        panel.border = element_rect(fill = NA, color = "black"),
+        strip.background = element_rect(color = NA, fill = NA)
+    ) +
+    guides(fill = "none") +
+    labs(x = "gene cluster", y = "genome")
 
 
+ggsave(paste0(folder_data, "temp/45-02-contig_gene.png"), p, width = 10, height = 6)
+
+# 3. make a tree
+# Compute the jaccard matrix
+library(proxy)
+egcc_c <- egcc_ordered %>%
+    # Filter for the largest three contigs
+    filter(contig_ordered %in% paste0("c", 1:3)) %>%
+    select(contig_id, gene_cluster_id) %>%
+    distinct(contig_id, gene_cluster_id) %>%
+    mutate(value = 1) %>%
+    arrange(contig_id, gene_cluster_id) %>%
+    pivot_wider(id_cols = contig_id, names_from = gene_cluster_id, values_from = value, values_fill = 0)
+
+# Compute the Jaccard distance matrix
+egcc_m <- as.matrix(egcc_c[,-1])
+dim(egcc_m)
+rownames(egcc_m) <- as.character(egcc_c$contig_id)
+jdm <- proxy::dist(egcc_m, method = "Jaccard")
+
+# Plot the tree
+library(ggtree)
+te <- as.dendrogram(hclust(jdm)) # my tree!
+egcc_label <- egcc_ordered %>% select(contig_id, genome_id, contig_ordered) %>% distinct() %>%
+    mutate(contig_id = as.character(contig_id))
 
 
+p <- te %>%
+    ggtree(layout = "rectangular") +
+    geom_tiplab() +
+    geom_highlight(data = egcc_label, aes(node = contig_id, fill = contig_ordered)) +
+    scale_x_continuous(expand = c(0.1,0)) +
+    scale_y_continuous(expand = c(0.1,0)) +
+    theme_tree2() +
+    theme(
+        plot.margin = unit(c(10, 10, 10, 10), "mm")
+    ) +
+    labs()
 
 
-
-
-
-
-
+ggsave(paste0(folder_data, "temp/45-03-contig_tree.png"), p, width = 10, height = 10)
 
 
 
