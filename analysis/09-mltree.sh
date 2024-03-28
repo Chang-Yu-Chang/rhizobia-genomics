f95af91e9b11e0077bbb106de5feb533f78ffcee
Chang-Yu Chang <changyuchang5@gmail.com>
2024-03-28 17:02:09 -0400

    run iqtree on core genes

---
+++ analysis/09-mltree.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env zsh
+source ~/.zshrc
+source 00-env_vars.sh
+
+# This script contrusted ML tree
+
+cd $folder_shell
+mkdir -p $folder_genomics/mltree
+
+
+mamba activate iqtree
+
+# Test on one gene
+aln='aarA'
+mkdir -p $folder_genomics/mltree/$aln
+iqtree \
+    -nt AUTO \
+    -s $folder_genomics/pangenome/panaroo/aligned_gene_sequences/$aln.aln.fas \
+    --prefix $folder_genomics/mltree/$aln/aln
+
+# All core genes
+mkdir -p $folder_genomics/mltree/core
+iqtree \
+    -nt AUTO \
+    -s $folder_genomics/pangenome/panaroo/core_gene_alignment.aln \
+    --prefix $folder_genomics/mltree/core/aln
