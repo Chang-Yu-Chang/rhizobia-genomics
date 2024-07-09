875a1b25abb398bf44ec40ea7ec56e5096d20f3d
Chang-Yu Chang <changyuchang5@gmail.com>
2024-07-09 11:29:21 -0400

    join contig gene content

--- genomics_analysis/contigs/clean_contigs.R
+++ genomics_analysis/contigs/clean_contigs.R
@@ -18,18 +18,19 @@ b_genome <- read_csv(paste0(folder_data, "genomics_analysis/taxonomy/b_genome.cs
 gpaf <- read_csv(paste0(folder_data, "genomics/pangenome/isolates/gene_presence_absence.csv")) %>%
     clean_names() %>%
     mutate(across(starts_with("g"), function (x) str_remove_all(x, "/Users/cychang/Dropbox/lab/local-adaptation/data/genomics/genomes/")))
-dim(gpaf) # 26886 genes in the pangenome
+dim(gpaf) # 26886 genes x 34-3 genes
 gd <- read_csv(paste0(folder_data, "genomics/pangenome/isolates/gene_data.csv")) %>%
     clean_names() %>%
     mutate(annotation_id = str_remove_all(annotation_id, "/Users/cychang/Dropbox/lab/local-adaptation/data/genomics/genomes/")) %>%
-    rename(genome_id = gff_file, contig_id = scaffold_name) %>%
+    rename(genome_id = gff_file, contig_id = scaffold_name, gene = gene_name) %>%
     mutate(contig_id = paste0(genome_id, "_", contig_id))
-dim(gd) # 225019 genes across genomes and which contig they are from. The longer format of gpaf
+dim(gd) # 225019 (gene x genome x contig) x 8
 
 # Number of genes
 gdn <- gd %>%
     group_by(genome_id, contig_id) %>%
     count(name = "n_genes")
+
 
 # Join the datasets
 contigs <- genomes %>%
@@ -40,26 +41,38 @@ contigs <- genomes %>%
 
 write_csv(contigs, paste0(folder_data, "genomics_analysis/contigs/contigs.csv"))
 
-# Join the by contig genes
+
+# Join by contig genes
 gpafl <- gpaf %>%
     mutate(gene_id = 1:n()) %>%
     select(gene_id, gene, starts_with("g")) %>%
     pivot_longer(cols = -c(gene_id, gene), names_to = "genome_id", values_to = "annotation_id", values_drop_na = T) %>%
     separate_rows(annotation_id, sep = ";")
+dim(gpafl) # 221882 x 4
 
-gpafl %>%
-    filter(gene_id==4)
+# Check the gene names
+length(unique(gpafl$gene)) # 26886 union genes
+length(unique(gd$gene)) # 6974
+
+unique(gpafl$gene)
+
+
+
 
 gpaflc <- gpafl %>%
-    left_join(select(gd, genome_id, contig_id, annotation_id))
-    # Somehow some gene are not matched?
+    left_join(select(gd, genome_id, contig_id, annotation_id)) %>%
     drop_na(contig_id)
+length(table(gpaflc$gene_id))
+
+sum(is.na(gpaflc$genome_id))
+sum(is.na(gpaflc$annotation_id))
+gpaflc %>% filter(is.na(contig_id))
+sum(is.na(gpaflc$contig_id)) # Somehow some genes are not matched?
 
 gpaflc %>%
     select(-gene) %>%
     group_by(contig_id) %>%
     count() %>%
-    view
     #select(gene_id, gene, contig_id, annotation_id) %>%
     #mutate(temp = 1) %>%
     pivot_wider(names_from = contig_id, values_from =  annotation_id)
--- genomics_analysis/gene_content/clean_data.R
+++ genomics_analysis/gene_content/clean_data.R
@@ -1,38 +1,37 @@
-#' This script cleans the gene presence/absence and compute distance
+#' This script cleans the gene presence/absence
 
 renv::load()
 library(tidyverse)
 library(janitor)
 source(here::here("metadata.R"))
 
+# Gene presence-absence table
 gpa <- read_delim(paste0(folder_data, "genomics/pangenome/isolates/gene_presence_absence.Rtab")) %>%
     clean_names() # Rows are genes, columns are genomes
+dim(gpa) # 26886 genes in union x 32-1 genomes
 write_csv(gpa, paste0(folder_data, "genomics_analysis/gene_content/gpa.csv"))
 
 # Transpose the gene presence-absence table
 gpat <- gpa %>%
     pivot_longer(cols = -gene, names_to = "genome_id") %>%
     pivot_wider(names_from = gene, values_from = value, values_fill = 0)
-
+dim(gpat) # 31 genomes x 26887 genes
 write_csv(gpat, paste0(folder_data, "genomics_analysis/gene_content/gpat.csv"))
 
 # Full directory for each gene
 gpaf <- read_csv(paste0(folder_data, "genomics/pangenome/isolates/gene_presence_absence.csv")) %>%
     clean_names() %>%
     mutate(across(starts_with("g"), function (x) str_remove_all(x, "/Users/cychang/Dropbox/lab/local-adaptation/data/genomics/genomes/")))
+dim(gpat) # 26886 genes x 34-3 genomes. 1) gene name 2) non unique gene name 3) annotation
 gd <- read_csv(paste0(folder_data, "genomics/pangenome/isolates/gene_data.csv")) %>%
     clean_names() %>%
-    mutate(annotation_id = str_remove_all(annotation_id, "/Users/cychang/Dropbox/lab/local-adaptation/data/genomics/genomes/"))
-
-# gpaf %>%
-#     select(gene, non_unique_gene_name, g2) %>%
-#     drop_na(g2)
-#
-# gd %>%
-#     filter(gff_file == "g2") %>%
-#     filter(annotation_id == "g2.fasta_00001")
+    mutate(annotation_id = str_remove_all(annotation_id, "/Users/cychang/Dropbox/lab/local-adaptation/data/genomics/genomes/")) %>%
+    rename(genome_id = gff_file, contig_id )
+dim(gd) # 225019 (gene x genome x contig) x 8 rows
 
 # Structural variation
-spa <- read_delim(paste0(folder_data, "genomics/pangenome/isolates/struct_presence_absence.Rtab"))
-
+spa <- read_delim(paste0(folder_data, "genomics/pangenome/isolates/struct_presence_absence.Rtab")) %>%
+    clean_names()
+dim(spa) # 9775 strcutural variants x 32-1 genomes
+write_csv(spa, paste0(folder_data, "genomics_analysis/gene_content/spa.csv"))
 
