30a99e70cffd3e9b4859644339e83471cedc2d9a
Chang-Yu Chang <changyuchang5@gmail.com>
2024-08-16 16:18:29 -0400

    assembly using correct flye parameters

--- genomics/assembly/denovo_assembly.sh
+++ genomics/assembly/denovo_assembly.sh
@@ -6,7 +6,8 @@ source ../env_vars.sh
 
 cd $folder_shell
 
-for i in 19 23 35 37
+#for i in {1..18} 20 21 22 {24..34} 36 38  # 19 23 35 37
+for i in {1..38}
 do
     echo $genome_ids[$i]
     dir=$folder_genomics/assembly/$genome_ids[$i]
@@ -17,25 +18,25 @@ do
     draft_genome=$dir/miniasm/draft_genome.fasta
     downsampled_reads2=$dir/downsampled_reads2.fastq
 
-    # # Downsample reads
-    # zsh 02a-downsample_reads.sh \
-    #     $filtered_reads \
-    #     $downsampled_reads
-    #
-    # # Draft genome via miniasm
-    # mkdir -p $dir/miniasm
-    # zsh 02b-draft_genome.sh \
-    #     $downsampled_reads \
-    #     $assembly_prefix \
-    #     $draft_genome
-    #
-    # # Downsample reads again
-    # genome_size=$(grep -v ">" $draft_genome | grep -E -o "G|C|T|A|N" | wc -l)
-    # zsh 02c-downsample_reads2.sh \
-    #     $genome_size \
-    #     $draft_genome \
-    #     $filtered_reads \
-    #     $downsampled_reads2
+    # Downsample reads
+    zsh 02a-downsample_reads.sh \
+        $filtered_reads \
+        $downsampled_reads
+
+    # Draft genome via miniasm
+    mkdir -p $dir/miniasm
+    zsh 02b-draft_genome.sh \
+        $downsampled_reads \
+        $assembly_prefix \
+        $draft_genome
+
+    # Downsample reads again
+    genome_size=$(grep -v ">" $draft_genome | grep -E -o "G|C|T|A|N" | wc -l)
+    zsh 02c-downsample_reads2.sh \
+        $genome_size \
+        $draft_genome \
+        $filtered_reads \
+        $downsampled_reads2
 
     # Assemble genome via flye
     mkdir -p $dir/flye
