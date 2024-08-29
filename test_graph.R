ce2816f8a4ee0ce7fa92bd8165f6d1ce8f1c589b
Chang-Yu Chang <changyuchang5@gmail.com>
2024-08-29 13:06:40 -0400

    make the pangenome fig

--- test_graph.R
+++
@@ -1,9 +0,0 @@
-
-library(igraph)
-renv::load()
-library(tidyverse)
-library(gggenomes)
-library(cowplot)
-source(here::here("metadata.R"))
-
-gg <- read_graph(paste0(folder_data, "genomics/pangenome/isolates/final_graph.gml"), format = "gml")
