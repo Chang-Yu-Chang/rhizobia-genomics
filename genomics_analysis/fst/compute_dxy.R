169fd0f5cc2b9d41cf854756b0b455d019aa4522
Chang-Yu Chang <changyuchang5@gmail.com>
2024-10-17 16:23:06 -0400

    compute dxy

--- genomics_analysis/fst/compute_dxy.R
+++ genomics_analysis/fst/compute_dxy.R
@@ -1,137 +1,91 @@
 #' This script computes Fst for a set of genes
 
 library(tidyverse)
-library(StAMPP)
-library(apex) # for reading multiple fasta
-library(ape)  # for genetic distance
-library(poppr) # for processing fasta
-library(mmod) # for computing Fst
-library(pegas)
+library(poppr) # for reading snps into a genind object and computing dxy
+library(ape) # for reading multiple fasta
 source(here::here("metadata.R"))
 
 isolates <- read_csv(paste0(folder_data, "mapping/isolates.csv"))
 genomes <- read_csv(paste0(folder_data, "mapping/genomes.csv"))
-isolates_tax <- read_csv(paste0(folder_data, "genomics_analysis/taxonomy/isolates_tax.csv"))
 
-compute_fst <- function (af1, n1, af2, n2, aft) {
-    #' Compute Fst from allele frequency
-    ht = 1-(aft^2+(1-aft)^2) # total heterozygosity
-    hs = n1/(n1+n2)*(1-af1^2-(1-af1)^2) + n2/(n1+n2)*(1-af2^2-(1-af2)^2) # within-population heterzygosity
-    fst = 1-(hs/ht)
-    return(tibble(ht = ht, hs = hs, fst = fst))
+read_aln <- function (aln_file) {
+    paste0(aln_file) %>%
+        read.dna(format = "fasta")
+}
+filter_snps <- function (genind_data, isolates) {
+    #' this function removes snps with missing data
+    # Clean index names
+    indNames(genind_data) <- str_remove(indNames(genind_data), ";\\w+") %>% str_remove("_R_")
+    # Assign population
+    isolates_pop <- tibble(genome_id = indNames(genind_data)) %>% left_join(isolates, by = join_by(genome_id))
+    pop(genind_data) <- isolates_pop$population
+    # Remove SNP with missing data
+    loci_to_keep <- apply(genind_data@tab, 2, function(x) !any(is.na(x)))
+    genind_data_filtered <- genind_data[,loci_to_keep]
 
-    # compute_fst(1/4, 4, 1/6, 6, 2/10)
-    # ht = 1-(0.8^2+0.2^2)
-    # hs = 0.4*(1-0.75^2-0.25^2) + 0.6*(1-(5/6)^2-(1/6)^2)
-    # fst = 1-(hs/ht)
+    return(genind_data_filtered)
 }
 
-
-# Example hypothetical data
-# Create a genotype matrix (rows are SNPs, columns are individuals)
-# 0 = homozygous for allele 1, 1 = heterozygous, 2 = homozygous for allele 2
-genotypes <- matrix(c(0, 1, 1, 0, 2, 0, 2, 1, 0, 1, 1, 0), nrow = 6, ncol = 2)
-
-# Assign populations (First 3 for population A, last 3 for population B)
-pop_labels <- c(rep("A", 3), rep("B", 3))
-
-# Create a genind object, if using adegenet package:
-library(adegenet)
-#genind_obj <- df2genind(genotypes, ploidy=2, ind.names=1: ncol(genotypes), loc.names=1:nrow(genotypes))
-
-# For this example, calculate Dxy manually
-calculate_dxy <- function(geno_matrix, popA, popB) {
-    n_sites <- nrow(geno_matrix)
-    n_A <- length(popA)
-    n_B <- length(popB)
-
-    total_dxy <- 0
-
-    for (i in 1:n_sites) {
-        # Get the alleles for this SNP in each population
-        alleles_A <- geno_matrix[i, popA]
-        alleles_B <- geno_matrix[i, popB]
-
-        # Calculate the number of differences
-        dxy <- sum(outer(alleles_A, alleles_B, "!=")) / (n_A * n_B)
-        total_dxy <- total_dxy + dxy
-    }
-
-    # Average Dxy across sites
-    normalized_dxy <- total_dxy / n_sites
-    return(normalized_dxy)
+make_longer_dist <- function (mdist) {
+    mdist %>%
+        as.matrix() %>%
+        as_tibble() %>%
+        mutate(genome_id1 = colnames(.)) %>%
+        pivot_longer(-genome_id1, names_to = "genome_id2", values_to = "dxy") %>%
+        filter(genome_id1 < genome_id2)
 }
 
-# Population indices (assuming columns 1-3 belong to Pop A and columns 4-6 to Pop B)
-popA_indices <- 1:3
-popB_indices <- 4:6
+compute_dxy <- function (gi_filtered) {
+    #' This compute the dxy between the two populations
+    ns <- table(gi_filtered@pop)
+    pop1 = names(ns)[1]
+    pop2 = names(ns)[2]
+    n1 = ns[1]
+    n2 = ns[2]
 
-# Calculate Dxy
-dxy_result <- calculate_dxy(genotypes, popA_indices, popB_indices)
-print(paste("Dxy:", dxy_result))
+    # number of nucleotide difference between sequences
+    pi1 <- mean(diss.dist(popsub(gi_filtered, pop1))) # Within pop1 dxy
+    pi2 <- mean(diss.dist(popsub(gi_filtered, pop2))) # Within pop2 dxy
+    dall <- bitwise.dist(gi_filtered, percent = F)
+    # mean(dall) should be the same as the sum of total heterozygosity across site sum(per_snp_fst$ht)
+    # Individual sample dxy
+    dall_ln <- make_longer_dist(dall)
+    # Between pops dxy
+    dxy <- mean(as.matrix(dall)[1:n1,(n1+1):(n1+n2)])
 
-
+    return(list(pop_dxy = tibble(pi1, pi2, dxy), ind_dxy = dall_ln))
+}
 
 for (set_name in c("elev_med", "urbn_mel")) {
-    set_name <- "elev_med"
+    #set_name <- "elev_med"
     #set_name <- "urbn_mel"
     list_sccg <- read_csv(paste0(folder_data, "genomics_analysis/gene_content/", set_name, "/list_sccg.csv"), col_names = "gene")
     gene_list <- list_sccg$gene
-    per_locus_fst_results <- list()
-    gene_wide_fst_results <- list()
+    pop_dxy_results <- list()
+    ind_dxy_results <- list()
 
-    # Step 2: Loop through each gene
     for (gene in gene_list) {
-        # Construct file name - assuming the files are named as "gene.fasta"
-        alignment_file <- paste0(folder_data, "genomics/pangenome/", set_name,"/aligned_gene_sequences/", gene, ".aln.fas")
+        # Read msa file
+        alignment <- read_aln(paste0(folder_data, "genomics/pangenome/", set_name,"/aligned_gene_sequences/", gene, ".aln.fas"))
 
-        # Step 3: Check if the file exists
-        if (file.exists(alignment_file)) {
-            # Read the alignment file
-            alignment <- read.dna(alignment_file, format = "fasta")
+        # Make a genind object
+        gi <- DNAbin2genind(alignment)
+        if (is.null(gi)) {cat(gene, "has no polymorphism. \n"); next}
 
-            # Convert to genind object
-            suppressMessages(genind_data <- DNAbin2genind(alignment))
+        # Remove SNPs with missing data
+        gi_filtered <- filter_snps(gi, isolates)
+        if(ncol(gi_filtered@tab) == 0) {cat(gene, "has no polymorphism after removing samples with missing data. \n"); next}
 
-            if (is.null(genind_data)) {cat(gene, "has no polymorphism. \n"); next}
+        # Compute pi and dxy
+        dd <- compute_dxy(gi_filtered)
+        pop_dxy_results[[gene]] <- dd$pop_dxy
+        ind_dxy_results[[gene]] <- dd$ind_dxy
 
-            # Clean header names
-            indNames(genind_data) <- str_remove(indNames(genind_data), ";\\w+") %>% str_remove("_R_")
-
-            # Assign populations
-            isolates_pop <- tibble(genome_id = indNames(genind_data)) %>% left_join(isolates, by = join_by(genome_id))
-            pop(genind_data) <- isolates_pop$site_group
-
-            # Remove SNP with missing data
-            loci_to_keep <- apply(genind_data@tab, 2, function(x) !any(is.na(x)))
-            genind_data_filtered <- genind_data[,loci_to_keep]
-            if(ncol(genind_data_filtered@tab) == 0) {cat(gene, "has no polymorphism after removing samples with missing data. \n"); next}
-
-            # Compute per locus nucleotide difference
-            snp_fst = list()
-            for (snp in unique(genind_data_filtered@loc.fac)) {
-                ii = str_detect(colnames(genind_data_filtered@tab), as.character(snp))
-                ss <- genind_data_filtered@tab[,ii]
-                pp <- tibble(population = pop(genind_data_filtered), allele1 = ss[,1], allele2 = ss[,2]) %>%
-                    mutate(allele1 = factor(allele1, c(0,1)))
-
-
-
-                snp_fst[[snp]] <- compute_dxy(pp$af[1], pp$n[1], pp$af[2], pp$n[2], sum(ss[,1])/length(ss[,1]))
-            }
-        }
+        cat("\nProcessed:", gene, "\n")
     }
+
+    gene_pop_dxy <- bind_rows(pop_dxy_results, .id = "gene")
+    gene_ind_dxy <- bind_rows(ind_dxy_results, .id = "gene")
+    write_csv(gene_pop_dxy, paste0(folder_data, "genomics_analysis/fst/", set_name, "/gene_pop_dxy.csv"))
+    write_csv(gene_ind_dxy, paste0(folder_data, "genomics_analysis/fst/", set_name, "/gene_ind_dxy.csv"))
 }
-
-
-
-# import genotype data and convert to allele frequecies
-data(potato.mini, package="StAMPP")
-potato.mini <- as_tibble(potato.mini)[,1:10]
-potato.freq <- stamppConvert(potato.mini, "r")
-dim(potato.freq)
-class(potato.mini)
-# Calculate genetic distance between individuals
-potato.D.ind <- stamppNeisD(potato.freq, FALSE)
-# Calculate genetic distance between populations
-potato.D.pop <- stamppNeisD(potato.freq, TRUE)
--- genomics_analysis/fst/compute_fst.R
+++ genomics_analysis/fst/compute_fst.R
@@ -1,115 +1,109 @@
 #' This script computes Fst for a set of genes
 
 library(tidyverse)
-library(apex) # for reading multiple fasta
-library(poppr) # for processing fasta
-library(mmod) # for computing Fst
-library(pegas)
+library(poppr) # for reading snps into a genind object
+library(ape) # for reading multiple fasta
 source(here::here("metadata.R"))
 
 isolates <- read_csv(paste0(folder_data, "mapping/isolates.csv"))
 genomes <- read_csv(paste0(folder_data, "mapping/genomes.csv"))
-isolates_tax <- read_csv(paste0(folder_data, "genomics_analysis/taxonomy/isolates_tax.csv"))
 
-compute_fst <- function (af1, n1, af2, n2, aft) {
-    #' Compute Fst from allele frequency
-    ht = 1-(aft^2+(1-aft)^2) # total heterozygosity
-    hs = n1/(n1+n2)*(1-af1^2-(1-af1)^2) + n2/(n1+n2)*(1-af2^2-(1-af2)^2) # within-population heterzygosity
+read_aln <- function (aln_file) {
+    paste0(aln_file) %>%
+        read.dna(format = "fasta")
+}
+filter_snps <- function (genind_data, isolates) {
+    #' this function removes snps with missing data
+    # Clean index names
+    indNames(genind_data) <- str_remove(indNames(genind_data), ";\\w+") %>% str_remove("_R_")
+    # Assign population
+    isolates_pop <- tibble(genome_id = indNames(genind_data)) %>% left_join(isolates, by = join_by(genome_id))
+    pop(genind_data) <- isolates_pop$population
+    # Remove SNP with missing data
+    loci_to_keep <- apply(genind_data@tab, 2, function(x) !any(is.na(x)))
+    genind_data_filtered <- genind_data[,loci_to_keep]
+
+    return(genind_data_filtered)
+}
+compute_af <- function (gi_filtered, snp) {
+    #' This function computes the allele frequency per population
+    gi_temp <- gi_filtered[, str_detect(colnames(gi_filtered@tab), snp)]
+    ns <- table(gi_temp@pop)
+    pop1 = names(ns)[1]
+    pop2 = names(ns)[2]
+    n1 = ns[1]
+    n2 = ns[2]
+    a1 = gi_temp@tab[which(gi_temp@pop == pop1),1]
+    af1 = mean(a1)
+    a2 = gi_temp@tab[which(gi_temp@pop == pop2),1]
+    af2 = mean(a2)
+    aft = mean(gi_temp@tab[,1])
+
+    return(tibble(af1, n1, af2, n2, aft))
+}
+compute_fst <- function (af1, n1, af2, n2, aft, fpc = T) {
+    #' Compute Fst from allele frequency. Same as poppr(gi)$Hexp
+
+    # Correct for finite/small population size
+    if (fpc) {
+        h1 = n1/(n1-1)*(1-af1^2-(1-af1)^2) # pop 1
+        h2 = n2/(n2-1)*(1-af2^2-(1-af2)^2) # pop 2
+        ht = (n1+n2)/(n1+n2-1)*(1-aft^2-(1-aft)^2) # total heterozygosity
+    } else if (fpc == F){
+        h1 = 1-af1^2-(1-af1)^2 # pop 1
+        h2 = 1-af2^2-(1-af2)^2 # pop 2
+        ht = 1-aft^2-(1-aft)^2 # total heterozygosity
+    }
+    hs = (n1*h1+n2*h2)/(n1+n2) # within-population heterozygosity
     fst = 1-(hs/ht)
-    return(tibble(ht = ht, hs = hs, fst = fst))
-
-    # compute_fst(1/4, 4, 1/6, 6, 2/10)
-    # ht = 1-(0.8^2+0.2^2)
-    # hs = 0.4*(1-0.75^2-0.25^2) + 0.6*(1-(5/6)^2-(1/6)^2)
-    # fst = 1-(hs/ht)
+    return(tibble(h1, h2, ht, hs, fst))
 }
 
 for (set_name in c("elev_med", "urbn_mel")) {
-#set_name <- "elev_med"
-#set_name <- "urbn_mel"
-list_sccg <- read_csv(paste0(folder_data, "genomics_analysis/gene_content/", set_name, "/list_sccg.csv"), col_names = "gene")
-gene_list <- list_sccg$gene
-per_locus_fst_results <- list()
-gene_wide_fst_results <- list()
+    #set_name <- "elev_med"
+    #set_name <- "urbn_mel"
+    list_sccg <- read_csv(paste0(folder_data, "genomics_analysis/gene_content/", set_name, "/list_sccg.csv"), col_names = "gene")
+    gene_list <- list_sccg$gene
 
-# Step 2: Loop through each gene
-for (gene in gene_list) {
-    # Construct file name - assuming the files are named as "gene.fasta"
-    alignment_file <- paste0(folder_data, "genomics/pangenome/", set_name,"/aligned_gene_sequences/", gene, ".aln.fas")
+    glen <- list()
+    per_locus_fst_results <- list()
+    gene_wide_fst_results <- list()
 
-    # Step 3: Check if the file exists
-    if (file.exists(alignment_file)) {
-        # Read the alignment file
-        alignment <- read.dna(alignment_file, format = "fasta")
+    # Loop through each gene
+    for (gene in gene_list) {
 
-        # Convert to genind object
-        suppressMessages(genind_data <- DNAbin2genind(alignment))
+        # Read msa file
+        alignment <- read_aln(paste0(folder_data, "genomics/pangenome/", set_name,"/aligned_gene_sequences/", gene, ".aln.fas"))
+        glen[[gene]] <- ncol(alignment) # sequence length
 
-        if (is.null(genind_data)) {cat(gene, "has no polymorphism. \n"); next}
+        # Make a genind object
+        gi <- DNAbin2genind(alignment)
+        if (is.null(gi)) {cat(gene, "has no polymorphism. \n"); next}
 
-        # Clean header names
-        indNames(genind_data) <- str_remove(indNames(genind_data), ";\\w+") %>% str_remove("_R_")
+        # Remove SNPs with missing data
+        gi_filtered <- filter_snps(gi, isolates)
+        if(ncol(gi_filtered@tab) == 0) {cat(gene, "has no polymorphism after removing samples with missing data. \n"); next}
 
-        # Assign populations
-        isolates_pop <- tibble(genome_id = indNames(genind_data)) %>% left_join(isolates, by = join_by(genome_id))
-        pop(genind_data) <- isolates_pop$site_group
-
-        # Remove SNP with missing data
-        loci_to_keep <- apply(genind_data@tab, 2, function(x) !any(is.na(x)))
-        genind_data_filtered <- genind_data[,loci_to_keep]
-        if(ncol(genind_data_filtered@tab) == 0) {cat(gene, "has no polymorphism after removing samples with missing data. \n"); next}
-
-        # Compute per locus F_st
-        snp_fst = list()
-        for (snp in unique(genind_data_filtered@loc.fac)) {
-            ii = str_detect(colnames(genind_data_filtered@tab), as.character(snp))
-            ss <- genind_data_filtered@tab[,ii]
-            pp <- tibble(population = pop(genind_data_filtered), allele1 = ss[,1], allele2 = ss[,2]) %>%
-                mutate(allele1 = factor(allele1, c(0,1))) %>%
-                group_by(population, allele1, .drop = F) %>%
-                count() %>%
-                group_by(population) %>%
-                mutate(af = n/sum(n), n = sum(n)) %>%
-                filter(allele1 == 1)
-
-            snp_fst[[snp]] <- compute_fst(pp$af[1], pp$n[1], pp$af[2], pp$n[2], sum(ss[,1])/length(ss[,1]))
+        # Compute per locus Fst
+        list_snps <- names(gi_filtered@all.names)
+        snp_fst <- list()
+        for (snp in list_snps) {
+            afs <- compute_af(gi_filtered, snp)
+            snp_fst[[snp]] <- compute_fst(afs$af1, afs$n1, afs$af2, afs$n2, afs$aft)
         }
-
         per_snp_fst <- bind_rows(snp_fst, .id = "location")
-        # # Compute per locus F_st
-        # fst <- diff_stats(genind_data_filtered)
 
         per_locus_fst_results[[gene]] <- per_snp_fst # per locus F_st results
-        gene_wide_fst_results[[gene]] <- summarize(per_snp_fst, fst = mean(fst)) # gene-wide F_st results
+        gene_wide_fst_results[[gene]] <- summarize(per_snp_fst, n_snps = n(), fst = mean(fst)) # gene-wide F_st results
 
-        # Optionally print progress
         cat("\nProcessed:", gene, "\n")
-    } else {
-        cat("File not found for gene:", gene, "\n")
     }
+
+    gene_lengths <- unlist(glen) %>% enframe(name = "gene", value = "sequence_length")
+    gene_wide_fst <- bind_rows(lapply(names(gene_wide_fst_results), function(gene) data.frame(gene = gene, fst = gene_wide_fst_results[[gene]]))) %>% as_tibble
+    per_locus_fst <- bind_rows(lapply(names(per_locus_fst_results), function(gene) data.frame(gene = gene, per_locus_fst_results[[gene]]))) %>% as_tibble
+
+    write_csv(gene_lengths, paste0(folder_data, "genomics_analysis/fst/", set_name, "/gene_lengths.csv"))
+    write_csv(gene_wide_fst, paste0(folder_data, "genomics_analysis/fst/", set_name, "/gene_wide_fst.csv"))
+    write_csv(per_locus_fst, paste0(folder_data, "genomics_analysis/fst/", set_name, "/per_locus_fst.csv"))
 }
-
-#gene_wide_fst <- bind_rows(lapply(names(gene_wide_fst_results), function(gene) data.frame(gene = gene, enframe(gene_wide_fst_results[[gene]])))) %>% as_tibble %>% pivot_wider
-gene_wide_fst <- bind_rows(lapply(names(gene_wide_fst_results), function(gene) data.frame(gene = gene, fst = gene_wide_fst_results[[gene]]))) %>% as_tibble
-per_locus_fst <- bind_rows(lapply(names(per_locus_fst_results), function(gene) data.frame(gene = gene, per_locus_fst_results[[gene]]))) %>% as_tibble
-
-write_csv(gene_wide_fst, paste0(folder_data, "genomics_analysis/fst/", set_name, "/gene_wide_fst.csv"))
-write_csv(per_locus_fst, paste0(folder_data, "genomics_analysis/fst/", set_name, "/per_locus_fst.csv"))
-
-
-
-# Get the sequence length
-glen <- list()
-for (gene in gene_list) {
-    alignment_file <- paste0(folder_data, "genomics/pangenome/", set_name,"/aligned_gene_sequences/", gene, ".aln.fas")
-    sequences <- Biostrings::readDNAStringSet(alignment_file)
-    glen[[gene]] <- length(sequences[[1]])
-    cat("Processed:", gene, "\n")
-}
-gene_lengths <- unlist(glen) %>% enframe(name = "gene", value = "sequence_length")
-write_csv(gene_lengths, paste0(folder_data, "genomics_analysis/fst/", set_name, "/gene_lengths.csv"))
-
-}
-
-#
-
