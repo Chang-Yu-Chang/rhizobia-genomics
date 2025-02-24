70ea2b4c5e65d0b7716e01f238f5454db3e6c52a
Chang-Yu-Chang <cychang2@sas-bio07420-md.bio.upenn.edu>
2025-02-24 18:11:39 -0500

    update script to reproduce conda

--- setup_conda.sh
+++ setup_conda.sh
@@ -14,10 +14,10 @@ rm -rf ~/miniforge
 
 # 2. Install miniconda for macOS with Apple M1 chip
 mkdir -p ~/miniconda3
-curl https://repo.anaconda.com/miniconda/Miniconda3-py311_23.7.4-0-MacOSX-arm64.sh -o ~/miniconda3/miniconda.sh
-zsh ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+curl https://repo.anaconda.com/miniconda/Miniconda3-py312_25.1.1-2-MacOSX-arm64.sh -o ~/miniconda3/miniconda.sh
+zsh ~/miniconda3/miniconda.sh -b -u -p ~/miniconda
 rm -rf ~/miniconda3/miniconda.sh
-~/miniconda3/bin/conda init zsh # Initiate the conda
+~/miniconda/bin/conda init zsh # Initiate the conda
 conda --version # this should return conda 23.7.4
 
 # 3. Create a conda env for osx-64 called intel_env
@@ -34,7 +34,7 @@ conda config --env --add channels bioconda
 conda config --env --add channels conda-forge
 
 # The .condarc should look like this
-cat ~/miniconda3/envs/intel_env/.condarc
+cat ~/miniconda/envs/intel_env/.condarc
 # subdir: osx-64
 # channels:
 #   - conda-forge
@@ -43,14 +43,15 @@ cat ~/miniconda3/envs/intel_env/.condarc
 
 # 4. Install mamba under the created conda env intel_env
 conda install -c conda-forge mamba
-mamba init
+mamba shell init
+mamba config --env --set subdir osx-64
 # Note that this command only modifies .bash_profile if you work in bash. For zsh environment, copy-paste the generated text from .bash_profile to .zshrc. Close and re-open a shell session. This step is essential for mamba activate to work
 mamba env list
 
 # 5. Install docker
-brew install docker # use homebrew to install docker
-cd ~/bioinformatics
-wget https://desktop.docker.com/mac/main/arm64/Docker.dmg?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-mac-arm64 -O Docker.dmg
-sudo hdiutil attach Docker.dmg
-sudo /Volumes/Docker/Docker.app/Contents/MacOS/install
-sudo hdiutil detach /Volumes/Docker
+# brew install docker # use homebrew to install docker
+# cd ~/bioinformatics
+# wget https://desktop.docker.com/mac/main/arm64/Docker.dmg?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-mac-arm64 -O Docker.dmg
+# sudo hdiutil attach Docker.dmg
+# sudo /Volumes/Docker/Docker.app/Contents/MacOS/install
+# sudo hdiutil detach /Volumes/Docker
