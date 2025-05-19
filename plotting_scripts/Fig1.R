7a60e420ea61519fca7db86e6d0eab75842c78eb
Chang-Yu-Chang <cychang2@sas-bio07420-md.bio.upenn.edu>
2025-05-19 18:35:08 -0400

    revise fig1

--- plotting_scripts/Fig1.R
+++ plotting_scripts/Fig1.R
@@ -3,15 +3,15 @@
 library(tidyverse)
 library(cowplot)
 library(ggh4x)
-library(ggrepel) # for annotating dots
-library(waffle) # for waffle plots
+library(tigris) # for getting the US state map
 library(sf) # for handling the simple features
 library(geodata) # for getting the elevation data
 library(stars) # for converting st to sf
 library(usdata) # for temperature. data
 library(ggspatial) # for scale bar
+library(ggrepel) # for annotating dots
+library(waffle) # for waffle plots
 source(here::here("metadata.R"))
-
 
 # Read data ----
 isolates <- read_csv(paste0(folder_data, "mapping/isolates.csv"))
@@ -21,11 +21,12 @@ sites  <- read_csv(paste0(folder_phenotypes, "sites/sites.csv")) %>%
     filter(population != "mid elevation") %>%
     # Use sites where the isolates were from
     filter(site %in% isolates$site) %>%
-    mutate(show = T) %>%
-    bind_rows(tibble(population = "PA", longitude_dec = c(-75.45, -75.05), elevation_m = 0, show = F)) %>%
-    bind_rows(tibble(population = "VA", longitude_dec = c(-80.75, -80.35), elevation_m = 0, show = F))
+    mutate(show = T)
+    # bind_rows(tibble(population = "PA", longitude_dec = c(-75.45, -75.05), elevation_m = 0, show = F)) %>%
+    # bind_rows(tibble(population = "VA", longitude_dec = c(-80.75, -80.35), elevation_m = 0, show = F))
 dml <- read_csv(paste0(folder_phenotypes, "sites/dml.csv"))
 tb_month <- read_csv(paste0(folder_phenotypes, "sites/tb_month.csv"))
+us_states <- states() # us state map
 # Daily max in the map region
 map_range <- read_csv(paste0(folder_phenotypes, "sites/map_range.csv")) %>%
     mutate(population = rep(c("PA", "VA"), each = 31*41))
@@ -33,92 +34,140 @@ dcl <- read_csv(paste0(folder_phenotypes, "sites/dcl.csv")) %>%
     left_join(map_range)
 
 # Panel A. map ----
-p1 <- dcl %>%
-    filter(yday >= 182, yday <= 273) %>%
-    group_by(population, grid, longitude, latitude) %>%
-    summarize(tmax = max(tmax_deg_c)) %>%
+sites_center <- sites %>%
+    drop_na(site) %>%
+    group_by(population) %>%
+    summarize(lat_mean = mean(latitude_dec), lon_mean = mean(longitude_dec), lat_max = max(latitude_dec), lat_min = min(latitude_dec), lon_max = max(longitude_dec), lon_min = min(longitude_dec)) %>%
+    mutate(hjust = c(0.8, 0.8)) %>%
+    mutate(width_max = max(c((lon_max-lon_min)*1.8, (lat_max-lat_min)*1.8)))
+
+p1 <- us_states %>%
+    filter(STUSPS %in% c("NY", "VA", "WV", "PA", "MD", "NJ", "DE", "KY", "OH", "NC", "MI", "IN", "CT", "MA", "RI")) %>%
     ggplot() +
-    geom_tile(aes(x = longitude, y = latitude, fill = tmax)) +
-    geom_point(data = sites, aes(x = longitude_dec, y = latitude_dec, color = show), fill = "white",  size = 2, shape = 21, stroke = 1) +
-    scale_fill_gradient2(low = "steelblue", high = "#db7272", mid = "snow", midpoint = 32, breaks = seq(10, 40, 2), name = "Daily maximum") +
-    scale_color_manual(values = c(`TRUE` = "black", `FALSE` = "white")) +
-    scale_x_continuous(expand = c(0,0), breaks = seq(-82, -71, .1)) +
-    scale_y_continuous(expand = c(0,0)) +
-    facet_wrap2(~population, scales = "free") +
-    coord_cartesian(clip = "off") +
-    theme_bw() +
+    geom_sf(fill = NA, color = "grey10", linewidth = .5) +
+    geom_tile(data = sites_center, aes(x = lon_mean, y = lat_mean, width = width_max, height = width_max), color = "black", fill = alpha("#FFC857", 0.5), linewidth = 0.5) +
+    annotation_scale(location = "bl", width_hint = .2) +
+    coord_sf(expand = F, xlim = c(-86.5, -70), ylim = c(35, 42.5)) +
+    theme_light() +
     theme(
-        legend.text = element_text(size = 8) ,
-        legend.title = element_text(size = 8),
-        legend.key.width = unit(5, "mm"),
-        legend.key.height = unit(8, "mm"),
-        legend.box.margin = unit(c(0,0,0,0), "mm"),
-        legend.position = "right",
-        strip.background = element_blank(),
-        panel.spacing.x = unit(3, "mm"),
-        panel.grid.minor = element_blank(),
-        #panel.border = element_rect(color = "black", fill = NA),
-        panel.border = element_rect(color = NA, fill = NA),
-        axis.title.x = element_blank(),
-        axis.text.x = element_blank(),
-        plot.margin = unit(c(0,0,0,0), "mm")
+        panel.background = element_rect(color = NA, fill = NA, linewidth = 1),
+        panel.border = element_rect(color = "black", fill = NA, linewidth = 1),
+        plot.background = element_rect(color = NA, fill = NA, linewidth = .5),
+        legend.position = "bottom",
+        panel.grid.major = element_blank(),
+        axis.line = element_line(linewidth = .5),
+        axis.ticks = element_line(color = "black", linewidth = .5),
+        axis.text = element_text(color = "black")
     ) +
-    guides(fill = guide_colorbar(barwidth = .8, barheight = 5), color = "none") +
-    labs(x = "Longitude", y = "Latitude", title = "")
+    guides(fill = "none", color = "none") +
+    labs(x = "Longitude", y = "Latitude")
+
+# Panel A inset map
+plot_temp_map <- function (dcl, sites, pop) {
+    #pop = "VA"
+    tb <- dcl %>%
+        filter(yday >= 182, yday <= 273) %>%
+        filter(population == pop) %>%
+        group_by(longitude, latitude) %>%
+        summarize(tmax = max(tmax_deg_c))
+    cell_size = .1
+
+    sf_map <- tb %>%
+        rowwise() %>%
+        mutate(
+            xmin = longitude - cell_size / 2,
+            xmax = longitude + cell_size / 2,
+            ymin = latitude - cell_size / 2,
+            ymax = latitude + cell_size / 2,
+            # Construct the polygon for each grid cell
+            geometry = list(st_polygon(list(rbind(
+                c(xmin, ymin),
+                c(xmax, ymin),
+                c(xmax, ymax),
+                c(xmin, ymax),
+                c(xmin, ymin)
+            ))))
+        ) %>%
+        ungroup() %>%
+        st_as_sf() %>%
+        st_set_crs(4326)
+
+    sf_map %>%
+        ggplot() +
+        geom_sf(aes(fill = tmax), color = NA) +
+        #geom_tile(aes(x = longitude, y = latitude, fill = tmax)) +
+        geom_point(data = filter(sites, population == pop), aes(x = longitude_dec, y = latitude_dec, color = show), fill = "white",  size = 2, shape = 21, stroke = 1) +
+        scale_fill_gradient2(low = "steelblue", high = "#db7272", mid = "snow", midpoint = 32, breaks = seq(28, 38, 2), limits = c(26, 38), name = "Daily maximum") +
+        scale_color_manual(values = c(`TRUE` = "black", `FALSE` = "white")) +
+        scale_x_continuous(expand = c(0,0)) +
+        scale_y_continuous(expand = c(0,0)) +
+        annotation_scale(location = "bl", width_hint = .2) +
+        coord_sf(expand = F) +
+        theme_bw() +
+        theme(
+            legend.text = element_text(size = 8) ,
+            legend.title = element_text(size = 8),
+            legend.key.width = unit(5, "mm"),
+            legend.key.height = unit(8, "mm"),
+            legend.box.margin = unit(c(0,0,0,0), "mm"),
+            legend.position = "right",
+            strip.background = element_blank(),
+            panel.spacing.x = unit(3, "mm"),
+            panel.grid.minor = element_blank(),
+            panel.border = element_rect(color = "black", fill = NA),
+            #axis.title.x = element_blank(),
+            axis.title = element_blank(),
+            axis.text = element_blank(),
+            axis.ticks = element_blank(),
+            plot.margin = unit(c(0,0,0,0), "mm"),
+            plot.background = element_blank()
+        ) +
+        guides(fill = guide_colorbar(barwidth = .8, barheight = 5), color = "none") +
+        labs(x = "Longitude", y = "Latitude", title = "")
+}
+p1_1 <- plot_temp_map(dcl, sites, "VA")
+p1_2 <- plot_temp_map(dcl, sites, "PA")
 
 
-# Panel B elevation ----
-p2 <- sites %>%
-    ggplot() +
-    geom_segment(aes(x = longitude_dec, xend = longitude_dec, y = 0, yend = elevation_m), alpha = .3) +
-    geom_point(aes(x = longitude_dec, y = elevation_m, color = show), fill = "white", shape = 21, stroke = 1, size = 2) +
-    geom_text_repel(aes(label = site, x = longitude_dec, y = elevation_m), nudge_x = 0) +
-    scale_color_manual(values = c(`TRUE` = "black", `FALSE` = "white")) +
-    scale_x_continuous(expand = c(0,0), breaks = seq(-82, -71, .1)) +
-    scale_y_continuous(expand = c(.1,0)) +
-    facet_wrap(~population, scales = "free") +
-    coord_cartesian(clip = "off") +
-    theme_bw() +
-    theme(
-        strip.text = element_blank(),
-        strip.background = element_blank(),
-        panel.grid.major.x = element_blank(),
-        panel.grid.minor.x = element_blank()
-    ) +
-    guides(color = "none") +
-    labs(x = "Longitude", y = "Elevation (m)")
-
-# Panel C. site temperature ----
+# Panel B. site temperature ----
 tb_tmax <- dml %>%
     filter(yday >= 182 & yday <= 273) %>%
     group_by(population) %>%
     summarize(mean_tmax = mean(tmax_deg_c))
 
-p3 <- dml %>%
+p2 <- dml %>%
     filter(yday >= 182 & yday <= 273) %>%
+    mutate(
+        population = factor(population, c("VA", "PA")),
+        site = factor(site, sites$site)
+    ) %>%
+    drop_na(site) %>%
     ggplot() +
     geom_histogram(aes(x = tmax_deg_c), position = "identity", alpha = .5, color = "black", fill = "white", binwidth = 1) +
-    geom_vline(data = tb_tmax, aes(xintercept = mean_tmax), linetype = 2) +
-    geom_text(data = tb_tmax, aes(label = paste0("mean=", round(mean_tmax, 2)), x = mean_tmax), y = 100, vjust = 2, hjust = 1) +
-    facet_grid2(~population, axes = "all") +
-    scale_x_continuous(limits = c(5, 40)) +
+    #geom_vline(data = tb_tmax, aes(xintercept = mean_tmax), linetype = 2) +
+    facet_nested(~population+site, nest_line = element_line(colour = "black")) +
+    scale_y_continuous(breaks = c(0, 10)) +
+    scale_x_continuous(breaks = seq(0, 40, 10), limits = c(5, 40), expand = c(0,0)) +
     coord_flip(clip = "off") +
-    theme_bw() +
+    theme_classic() +
     theme(
         axis.title = element_text(size = 10),
         strip.background = element_blank(),
-        strip.text = element_blank(),
-        panel.spacing.x = unit(6, "mm"),
-        panel.grid.minor = element_blank()
+        panel.spacing.x = unit(2, "mm"),
+        panel.grid.major.y = element_line(color = "grey90", linewidth = .5),
+        panel.grid.minor.y = element_line(color = "grey90", linewidth = .3)
     ) +
     guides() +
     labs(x = expression("Daily maximum "(degree*C)), y = "Num. of days in Jul-Sep")
 
-# Panel D. strain composition ----
-p4 <- iso %>%
+# Panel C. strain composition ----
+p3 <- iso %>%
     left_join(select(isolates, population, site, genome_id)) %>%
     select(population, contig_species, site) %>%
-    mutate(site = factor(site, c("fp", "src", "crp", "gp", "bg", "40th", "pms", "ppf", "L1", "L2", "H4", "H3", "H2", "L3", "L4"))) %>%
+    mutate(
+        population = factor(population, c("VA", "PA")),
+        site = factor(site, sites$site)
+    ) %>%
     group_by(population, site, contig_species) %>%
     count() %>%
     ggplot(aes(fill = contig_species, values = n)) +
@@ -131,7 +180,7 @@ p4 <- iso %>%
         panel.grid.minor = element_blank(),
         panel.grid.major.x = element_blank(),
         panel.border = element_blank(),
-        panel.spacing.x = unit(1, "mm"),
+        panel.spacing.x = unit(2, "mm"),
         axis.text.x = element_blank(),
         axis.ticks.x = element_blank(),
         strip.clip = "off",
@@ -139,25 +188,28 @@ p4 <- iso %>%
         #strip.text = element_blank(),
         legend.position = "right",
         legend.title = element_blank(),
+        legend.background = element_rect(color = "black", fill = NA),
         plot.margin = unit(c(0, 12, 0, 15), unit = "mm")
     ) +
-    guides(fill = guide_legend(nrow = 2)) +
+    guides(fill = guide_legend(ncol = 1)) +
     labs()
 
 # ----
-p_left <- plot_grid(
-    p1, p2, p3, p4,
-    ncol = 1, align = "v", axis = "rl", rel_heights = c(1.5,1,1,1),
-    labels = LETTERS[1:4]
+p_main <- plot_grid(
+    p1, p2, p3,
+    scale = .95,
+    ncol = 1, align = "v", axis = "rl", rel_heights = c(1.5,1,.8),
+    labels = LETTERS[1:3]
 ) +
     theme(plot.background = element_rect(color = NA, fill = "white"))
 
 p <- ggdraw() +
-    draw_plot(p_left, width = 1, height = 1) +
-    #draw_image(here::here("plots/cartoons/Fig1.png"), scale = 1) +
+    draw_plot(p_main) +
+    draw_plot(p1_1 + guides(fill = "none"), x = .1, y = .7, scale = .2, halign = 0, valign = 0) +
+    draw_plot(p1_2 + guides(fill = "none"), x = .45, y = .6, scale = .2, halign = 0, valign = 0) +
     theme(plot.background = element_rect(color = NA, fill = "white"))
 
-ggsave(here::here("plots/Fig1.png"), p, width = 9, height = 9)
+ggsave(here::here("plots/Fig1.png"), p, width = 8, height = 8)
 
 # Chisquare
 x <- iso %>%
